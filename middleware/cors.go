318dbd0f012d932d621a49c358f5206a4f34cc2f
BO <2396768163@qq.com>
2025-12-29 22:29:08 +0800

    2025-12-29

--- middleware/cors.go
+++ middleware/cors.go
@@ -1,10 +1,14 @@
 package middleware
 
-import "github.com/gin-gonic/gin"
+import (
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
 
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Header("Access-Control-Allow-Origin", "https://xbzhong.cn")
+		c.Header("Access-Control-Allow-Origin", os.Getenv("BASE_URL"))
 		c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 		c.Header("Access-Control-Allow-Headers", "Content-Type, Authorization, x-vercel-ip, x-vercel-ip-city")
 
