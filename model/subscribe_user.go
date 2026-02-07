76cf0a7e407f9db44ed3b4f706dc92c272136e14
BO <2396768163@qq.com>
2026-02-07 19:32:36 +0800

    feat: added subscribe_user table definition

--- model/subscribe_user.go
+++ model/subscribe_user.go
@@ -1,1 +1,17 @@
 package model
+
+import "time"
+
+type SubscribeUser struct {
+	ID            int64     `gorm:"primaryKey;autoIncrement;column:id"`
+	Telephone     string    `gorm:"column:telephone;not null;unique"`
+	Email         string    `gorm:"column:email;type:text;not null"`
+	VisitorID     string    `gorm:"column:visitor_id;type:text;not null"`
+	SubscribeTime time.Time `gorm:"column:subscribe_time;type:datetime;not null"`
+	Status        uint8     `gorm:"column:status;type:tinyint(1);not null"`
+	UpdateAt      time.Time `gorm:"column:update_at;type:datetime"`
+}
+
+func (s SubscribeUser) TableName() string {
+	return "subscribe_user"
+}
